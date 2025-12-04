7ba4808a346191b48dd1cf02c611c4ac4a8b0b22
César Ferreira <cesarferreiraa67@gmail.com>
2025-12-03 22:28:41 -0300

    refactor

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Livewire\Admin\Auth\Login;
+use App\Livewire\Admin\Auth\Logout;
+use App\Livewire\Admin\Home\IndexHome;
+use App\Livewire\Admin\Users\IndexUsers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+})->name('home');
+
+Route::middleware('guest')->prefix('admin')->group(function () {
+    Route::get('login', Login::class)->name('login');
 });
+
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::get('users', IndexUsers::class)->name('users.index');
+    Route::get('home', IndexHome::class)->name('home.index');
+});
+
+Route::post('logout', Logout::class)
+    ->name('logout');
