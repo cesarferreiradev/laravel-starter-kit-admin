04a8b547f46e224e7018e73a2525f74484624c36
César Ferreira <cesarferreiraa67@gmail.com>
2025-12-04 20:58:38 -0300

    Update login.blade.php

--- resources/views/livewire/admin/auth/login.blade.php
+++ resources/views/livewire/admin/auth/login.blade.php
@@ -9,38 +9,23 @@
                             <p class="text-slate-600 text-[15px] mt-6 leading-relaxed">Faça login na sua conta e explore toda a gestão do seu condomínio</p>
                         </div>
 
-                        <div>
-                            <label class="text-slate-900 text-sm font-medium mb-2 block">Email</label>
-                            <div class="relative flex items-center">
-                                <input type="email" wire:model="email" required class="w-full text-sm text-slate-900 border border-slate-300 pl-4 pr-10 py-3 rounded-lg outline-blue-600" placeholder="Insira seu email" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-4" viewBox="0 0 24 24">
-                                    <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
-                                    <path d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z" data-original="#000000"></path>
-                                </svg>
-                            </div>
-
-                            @error('email')
-                                <span class="mt-2 text-red-500 text-xs">{{ $message }}</span>
-                            @enderror
-
-                        </div>
-                        <div>
-                            <label class="text-slate-900 text-sm font-medium mb-2 block">Senha</label>
-                            <div class="relative flex items-center">
-                                <input type="password" wire:model="password" required class="w-full text-sm text-slate-900 border border-slate-300 pl-4 pr-10 py-3 rounded-lg outline-blue-600" placeholder="Insira sua senha" />
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" class="w-[18px] h-[18px] absolute right-4 cursor-pointer" viewBox="0 0 128 128">
-                                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" data-original="#000000"></path>
-                                </svg>
+                        <div class="row space-y-6">
+                            <div class="col-12">
+                                <x-ts-input label="Email *" required wire:model="email" />
+                            </div><div class="col-12">
+                                <x-ts-password label="Senha *" required wire:model="password" />
                             </div>
                         </div>
-                        <div class="flex flex-wrap items-center justify-between gap-4">
+
+                        <!-- Todo: Recuperar senha -->
+                        {{--<div class="flex flex-wrap items-center justify-between gap-4">
 
                             <div class="text-sm">
-                                <a href="jajvascript:void(0);" class="text-blue-600 hover:underline font-medium">
+                                <a  class="text-blue-600 hover:underline font-medium">
                                     Esqueci minha senha
                                 </a>
                             </div>
-                        </div>
+                        </div>--}}
 
                         <div class="!mt-12">
                             <button type="submit" class="w-full shadow-xl py-2.5 px-4 text-[15px] font-medium tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none cursor-pointer">
