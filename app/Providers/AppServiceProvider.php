745361b8248e75da1d26c0c886017d9375b70445
César Ferreira <cesarferreiraa67@gmail.com>
2025-12-13 17:04:25 -0300

    refactor: Styling of the toast and table components in the tall stack ui

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TallStackUi\Facades\TallStackUi;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TallStackUi::personalize()
+            ->toast()
+            ->block('wrapper.first', 'pointer-events-none fixed top-16 z-50 right-[-70px] min-w-[40%] gap-y-2 px-4 py-4 flex flex-col gap-y-2');
+
+        TallStackUi::personalize()
+            ->stats()
+            ->block('wrapper.first','dark:bg-dark-700 flex w-full flex-col rounded-lg bg-white border border-slate-200');
     }
 }
